24a61dba3416d624f9afb6da5d573c0ae864f3bd
dariasavitskaia <libtech1991@gmail.com>
2022-12-01 22:08:58 +0300

    homepage

---
+++ public/homePage.js
@@ -0,0 +1,100 @@
+const logoutButton = new LogoutButton();
+logoutButton.action = () => {
+  ApiConnector.logout((f) => location.reload());
+};
+
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
+
+const ratesBoard = new RatesBoard();
+
+function getCurrencies() {
+  ApiConnector.getStocks(() => {
+    if (response.success) {
+      ratesBoard.clearTable();
+      ratesBoard.fillTable(response.data);
+    }
+  });
+}
+getCurrencies();
+setInterval(getCurrencies, 60000);
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = function (data) {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      this.setMessage(response.success, "Пополнение выполнено успешно");
+    } else {
+      this.setMessage(response.success, response.error);
+    }
+  });
+};
+
+moneyManager.conversionMoneyCallback = function (data) {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      this.setMessage(response.success, "Конвертация успешно выполнена");
+    } else {
+      this.setMessage(response.success, response.error);
+    }
+  });
+};
+
+moneyManager.sendMoneyCallback = function (data) {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      this.setMessage(response.success, "Перевод успешно выполнен");
+    } else {
+      this.setMessage(response.success, response.error);
+    }
+  });
+};
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+  if (response.success) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+  }
+});
+
+favoritesWidget.addUserCallback = function (data) {
+  ApiConnector.addUserToFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      this.setMessage(
+        response.success,
+        "Пользователь успешно добавлен в избранное"
+      );
+    } else {
+      this.setMessage(response.success, response.error);
+    }
+  });
+};
+
+favoritesWidget.removeUserCallback = function (data) {
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    if (response.success) {
+      favoritesWidget.clearTable();
+      favoritesWidget.fillTable(response.data);
+      moneyManager.updateUsersList(response.data);
+      this.setMessage(
+        response.success,
+        "Пользователь успешно удален из избранного"
+      );
+    } else {
+      this.setMessage(response.success, response.error);
+    }
+  });
+};
